9e5a233c7601122f7c0dab70ff3a5ade338a2bc7
mostafaEmad123456789 <mostafa5mes@gmail.com>
2021-07-25 18:30:13 +0200

    Fix Worksapace Errors

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 
-WORKSPACE=/home/mostafa/jenkins-data/jenkins_home/workspace/pipleline-maven
 
 #Copy the jar file
 cp -f java-app/target/*.jar jenkins/build/ 
  
 
 echo '*************Building the Docker Image***************'
-cd $WORKSPACE/jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache 
+cd /jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache 
 
 
